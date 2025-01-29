0d2b8685a46d9fd00c210b7ff308f0e8baa847ba
suvankargiri7 <suvankargiri7@gmail.com>
2025-01-29 11:34:37 +0530

    fix: wallet connected with browser reload and store wallet connection status save in local storage

--- src/Hooks/useWallet.ts
+++ src/Hooks/useWallet.ts
@@ -1,46 +1,109 @@
-import { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { 
-  connectWalletStart, 
-  connectWalletSuccess, 
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  connectWalletStart,
+  connectWalletSuccess,
   connectWalletFailure,
   disconnectWallet,
-} from '../store/wallet/walletSlice';
-import { RootState } from '../store/store';
-import { useNetwork } from './useNetwork';
+} from "../store/wallet/walletSlice";
+import { RootState } from "../store/store";
+import { useNetwork } from "./useNetwork";
+import { LOCAL_STORAGE_KEYS } from "../constants/localstoragekeys";
 
 export const useWallet = () => {
   const dispatch = useDispatch();
   const { isConnected, address, loading, error } = useSelector(
     (state: RootState) => state.wallet
   );
-  const { connect: connectNetwork, disconnect: disconnectNetwork } = useNetwork();
+  const { connect: connectNetwork, disconnect: disconnectNetwork } =
+    useNetwork();
 
   const connectWallet = useCallback(async () => {
     dispatch(connectWalletStart());
     try {
-      if (typeof window.ethereum !== 'undefined') {
-        const accounts = await window.ethereum.request({ 
-          method: 'eth_requestAccounts' 
+      if (typeof window.ethereum !== "undefined") {
+        //check if the user has already connected to the wallet
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
         });
-        dispatch(connectWalletSuccess(accounts[0]));
+
+        if (accounts.length > 0) {
+          dispatch(connectWalletSuccess(accounts[0]));
+          // Save connection state
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_CONNECTED, "true");
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_ADDRESS, accounts[0]);
+          connectNetwork();
+          return;
+        }
+
+        const newAccounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        dispatch(connectWalletSuccess(newAccounts[0]));
+
+        // Save connection state
+        localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_CONNECTED, "true");
+        localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_ADDRESS, accounts[0]);
+
         connectNetwork();
       } else {
-        throw new Error('Please install MetaMask'); 
+        throw new Error("Please install MetaMask");
       }
     } catch (error) {
-      dispatch(connectWalletFailure(error instanceof Error ? error.message : 'Failed to connect wallet'));
+      dispatch(
+        connectWalletFailure(
+          error instanceof Error ? error.message : "Failed to connect wallet"
+        )
+      );
     }
   }, [dispatch, connectNetwork]);
 
   const disconnect = useCallback(() => {
     dispatch(disconnectWallet());
+    localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_CONNECTED, "false");
+    localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_ADDRESS, "");
     disconnectNetwork();
   }, [dispatch, disconnectNetwork]);
 
   const displayBalance = useCallback(() => {
     console.log("displayBalance");
   }, [dispatch, connectNetwork]);
+
+  // Add auto-connect on app load
+  useEffect(() => {
+    const wasConnected = localStorage.getItem(
+      LOCAL_STORAGE_KEYS.WALLET_CONNECTED
+    );
+    if (wasConnected === "true") {
+      connectWallet();
+    }
+  }, [connectWallet]);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      const handleAccountsChanged = (accounts: string[]) => {
+        if (accounts.length === 0) {
+          // User disconnected
+          disconnect();
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_CONNECTED, "false");
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_ADDRESS, "");
+        } else {
+          // Account changed
+          dispatch(connectWalletSuccess(accounts[0]));
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_CONNECTED, "true");
+          localStorage.setItem(LOCAL_STORAGE_KEYS.WALLET_ADDRESS, accounts[0]);
+        }
+      };
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+      // Cleanup
+      return () => {
+        window.ethereum?.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      };
+    }
+  }, [dispatch]);
 
   return {
     isConnected,
@@ -49,6 +112,6 @@ export const useWallet = () => {
     error,
     connectWallet,
     disconnect,
-    displayBalance
+    displayBalance,
   };
 };
---
+++ src/constants/localstoragekeys.ts
@@ -0,0 +1,12 @@
+const WALLET_CONNECTED: string = 'wallet_connected'; 
+const WALLET_DISCONNECTED: string = 'wallet_disconnected';
+const WALLET_ADDRESS: string = 'wallet_address';
+const WALLET_NETWORK: string = 'wallet_network';
+
+export const LOCAL_STORAGE_KEYS = {
+  WALLET_CONNECTED,
+  WALLET_DISCONNECTED,
+  WALLET_ADDRESS,
+  WALLET_NETWORK
+};
+
